a78ad00f74eabf4b56246f642b7da40ac7693c85
“Ahad <ahussain@dev-10.com>
2022-04-08 12:21:45 -0400

    Put file reading in functions and read/cleaned highfiber.txt

--- foods-exercise.py
+++ foods-exercise.py
@@ -1,12 +1,33 @@
-f = open("foods.txt","r")
-foods = [line for line in f.read().splitlines()[1:] if line.strip()] # Remove blank spaces
-f.close()
-food_list = []
+def foods_file():
+    f = open("foods.txt","r")
+    foods = [line for line in f.read().splitlines()[1:] if line.strip()] # Remove blank spaces
+    f.close()
+    food_list = []
 
-for food in foods:
-    food = ''.join('' if letter == '&' else letter for letter in food)
-    food = ''.join('' if letter.isnumeric() else letter for letter in food)
+    for food in foods:
+        food = ''.join('' if letter == '&' else letter for letter in food)
+        food = ''.join('' if letter.isnumeric() else letter for letter in food)
 
-    food_list.append(food.title())
+        food_list.append(food.title())
 
-print(food_list)
+    return food_list
+
+
+def high_fiber_file():
+    f = open("highfiber.txt","r")
+    fibers = [line for line in f.read().splitlines()[1:] if line.strip()] # Remove blank spaces
+    f.close()
+    fiber_list = []
+
+    for fiber in fibers:
+        if fiber == "23314":
+            fiber == 'yes'
+        fiber = ''.join('o' if letter == '0' else letter for letter in fiber)
+        fiber = ''.join('' if letter == ' ' else letter for letter in fiber)
+        fiber = ''.join('' if letter.isnumeric() else letter for letter in fiber)
+        fiber_list.append(fiber.title())
+
+    return fiber_list
+
+print(len(high_fiber_file()))
+print(len(foods_file()))
