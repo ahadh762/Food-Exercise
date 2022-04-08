ca9508dfd49a485276e66148e3e73f30b609edf9
“Ahad <ahussain@dev-10.com>
2022-04-08 12:35:59 -0400

    Read low-glycemic-index.txt file

--- foods-exercise.py
+++ foods-exercise.py
@@ -5,10 +5,14 @@ def foods_file():
     food_list = []
 
     for food in foods:
-        food = ''.join('' if letter == '&' else letter for letter in food)
+        if '&' in list(food):
+            food = ""
         food = ''.join('' if letter.isnumeric() else letter for letter in food)
 
         food_list.append(food.title())
+
+        if food == "":
+            food_list.remove(food)        
 
     return food_list
 
@@ -20,14 +24,34 @@ def high_fiber_file():
     fiber_list = []
 
     for fiber in fibers:
-        if fiber == "23314":
-            fiber == 'yes'
         fiber = ''.join('o' if letter == '0' else letter for letter in fiber)
         fiber = ''.join('' if letter == ' ' else letter for letter in fiber)
         fiber = ''.join('' if letter.isnumeric() else letter for letter in fiber)
         fiber_list.append(fiber.title())
+        if fiber == "":
+            fiber_list.remove(fiber)
 
     return fiber_list
 
+
+def low_glycemic_index_file():
+    f = open("low-glycemic-index.txt","r")
+    glycemic_index = [line for line in f.read().splitlines()[1:] if line.strip()] # Remove blank spaces
+    f.close()
+    GI_list = []
+
+    for GI in glycemic_index:
+        if len(GI) > 3:
+            GI = ""
+        GI = ''.join('o' if letter == '0' else letter for letter in GI)
+        GI = ''.join('' if letter == ' ' else letter for letter in GI)
+        GI = ''.join('' if letter.isnumeric() else letter for letter in GI)
+        GI_list.append(GI.title())
+        if GI == "":
+            GI_list.remove(GI)
+
+    return GI_list
+
 print(len(high_fiber_file()))
 print(len(foods_file()))
+print(len(low_glycemic_index_file()))
